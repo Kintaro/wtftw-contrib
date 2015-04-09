854e17608f5b2fe7a9ff2cd852cae3ec90bf4339
Simon Wollwage <mail.wollwage@gmail.com>
2015-04-09 22:33:07 +0900

    reexported layouts

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -8,3 +8,14 @@ pub mod mirror_layout;
 pub mod resizable_tall_layout;
 pub mod with_borders_layout;
 pub mod no_borders_layout;
+
+pub use layout::avoid_struts_layout::*;
+pub use layout::binary_space_partition::*;
+pub use layout::center_layout::*;
+pub use layout::full_layout::*;
+pub use layout::gap_layout::*;
+pub use layout::layout_collection::*;
+pub use layout::mirror_layout::*;
+pub use layout::resizable_tall_layout::*;
+pub use layout::with_borders_layout::*;
+pub use layout::no_borders_layout::*;
